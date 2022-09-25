5e67b6e878fcdbdfcb3da65119cb0a37c9b8b3a5
carloslpsouza <carlos.souza@unigranrio.br>
2022-09-25 15:25:11 -0300

    Criação de arquivos base de cor e mensagens

---
+++ src/styles/especColors.ts
@@ -0,0 +1,9 @@
+export const especColors = {
+    risco: {
+        naoUrgencia: '#264474',
+        poucaUrgencia: "#007E49",
+        urgencia: "#FECB0A",
+        muitaUrgencia: "#F38221",
+        emergencia: "#EC1F25"
+    }
+}
---
+++ src/utils/mensagensPadrao.tsx
@@ -0,0 +1,9 @@
+export const msg = {
+    risco: {
+        naoUrgencia: 'Não urgente',
+        poucaUrgencia: "Pouca urgência",
+        urgencia: "Urgente",
+        muitaUrgencia: "Muito urgente",
+        emergencia: "EMERGÊNCIA"
+    }
+}
